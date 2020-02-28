929ba3935f012f68d8ac6d2aeef553557279655b
sandrodav <sndrodavadze@gmail.com>
2020-02-28 18:37:55 +0400

    corect version

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :tables
-  resources :meals
-  devise_for :users
-  root to: 'tables#index'
-  get '/search' , to: 'meals#search'
   resources :users do
     member do
       get :admin
@@ -12,5 +6,18 @@ Rails.application.routes.draw do
       get :usual
     end
   end
+  resources :tables
+  resources :meals
+  devise_for :users, :path_prefix => 'my'
+  resources :users do
+    member do
+      get :admin
+      get :manager
+      get :usual
+    end
+  end
+  root to: 'tables#index'
+  get '/search' , to: 'meals#search'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
